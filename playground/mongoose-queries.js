a56a81fd47020ce4fc32aba454dc92482c98aa56
Ben Terry <beterry12@gmail.com>
2019-02-12 11:08:43 -0500

    add mongoose-queries.js to playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,30 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// let id = '5c62e56ea4007b0b94b528a7';
+//
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+//
+// Todo.findById(id).then((todo) => {
+//   console.log('Todo by ID', todo);
+// })
+
+let id = '5c5a25817d84242a2428141b';
+
+User.findById(id).then((user) => {
+  if (!user){
+    console.log('No user with that id');
+  }
+  console.log('User:', user);
+});
