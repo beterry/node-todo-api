548ef7009f5f61ccaa2067af9736aee1b2744925
Ben Terry <beterry12@gmail.com>
2019-02-12 11:53:17 -0500

    Add GET todos/:id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -4,6 +4,10 @@ const {User} = require('./../server/models/user');
 
 // let id = '5c62e56ea4007b0b94b528a7';
 //
+// if (!ObjectID.isValid(id)){
+//   console.log('ID is not valid');
+// }
+//
 // Todo.find({
 //   _id: id
 // }).then((todos) => {
@@ -24,7 +28,7 @@ let id = '5c5a25817d84242a2428141b';
 
 User.findById(id).then((user) => {
   if (!user){
-    console.log('No user with that id');
+    return console.log('No user with that id');
   }
   console.log('User:', user);
 });
